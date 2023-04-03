026ea2a18f7ddb55a9127c86633c22e4e8d67cb5
Saad Siddiqui <saadsidd2002@gmail.com>
2023-04-03 02:20:05 -0400

    Testing communication to database

---
+++ app/src/main/java/com/example/sharearide/RegisterActivity.java
@@ -0,0 +1,29 @@
+package com.example.sharearide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        configureButton();
+    }
+
+    private void configureButton(){
+        Button loginButton = (Button) findViewById(R.id.loginbtn);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+    }
+}
