c4ff4833dcc4136d2a1688aee4425744ca0d25b5
Saad Siddiqui <saadsidd2002@gmail.com>
2023-04-09 22:40:15 -0400

    Clean up

--- app/src/main/java/com/example/sharearide/utils/Constants.java
+++ app/src/main/java/com/example/sharearide/utils/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
     private static final String LOCAL_URL = "http://10.0.2.2:5050/";
     private static final String PUBLIC_URL = "https://sharearide-backend-production.up.railway.app/";
 
-    public final static String URL = LOCAL_URL;
+    public final static String URL = PUBLIC_URL;
     public final static String LOGIN = "login/";
     public final static String REGISTER = "registeraccount/";
 }
